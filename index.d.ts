afb820de307f33ff33c812e9baf7ad83319915ff
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2020-10-16 00:29:21 +0300

    1.0.1

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { PKCEHelper } from 'abstract-pkce';
 export type { PKCEHelper, PKCEChallenge } from 'abstract-pkce';
-export declare type PKCEHelperLegacyBrowser = PKCEHelper<Promise<string>>;
+export declare type PKCEHelperWebCrypto = PKCEHelper<Promise<string>>;
 export declare type PKCEEncoding = 'hex' | 'base64';
-export declare const createPKCEHelper: (algorithm: string, encoding: PKCEEncoding, isHMAC?: boolean) => PKCEHelperLegacyBrowser;
+export declare const createPKCEHelper: (algorithm: string, encoding: PKCEEncoding, isHMAC?: boolean) => PKCEHelperWebCrypto;
